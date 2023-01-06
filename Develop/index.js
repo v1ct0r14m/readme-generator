570b354b7b8fb7a5ea928dc9648fc7afe3ec0e56
v1ct0r14m <tori.a.g.mota@gmail.com>
2023-01-06 02:00:41 -0600

    trying best, setting up prompts for inquirer

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,34 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer')
+const fs = require('fs')
+const pageTemplate = require('./develop/pageTemplate')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'username',
+            message: 'provide your github username',
+            validate: userInput => {
+                if (userInput) {return true}
+                else {
+                    console.log('enter your github username')
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'provide you email address'
+            validate: userEmail => {
+                if (userEmail) {return true}
+                else {
+                    console.log('enter your email address')
+                }
+            }
+        },
+    ])
+}    
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
